7beb28b25c4f29815919aa722bf8ec35baf16e99
GBTC440019UR <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-28 16:26:40 -0500

    3rd commit

--- src/main/java/com/example/demo/CategoryRepository.java
+++ src/main/java/com/example/demo/CategoryRepository.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 }
